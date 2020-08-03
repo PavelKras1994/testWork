55f25e64f52cdcb20da4a62e176ad683a7fa9f54
Pavel <pavekrasnoschok@gmai.com>
2020-08-03 17:48:57 +0300

    init gh-pages

---
+++ build/precache-manifest.5f36380a725756aa28a93350479c4740.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aa36f4ae05f1200a9b1dc66ca9f808b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49fc56b2a542f52c796a",
+    "url": "/static/css/main.5b9e4e90.chunk.css"
+  },
+  {
+    "revision": "bfb63b978397eb52c3ee",
+    "url": "/static/js/2.a26bd395.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a26bd395.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49fc56b2a542f52c796a",
+    "url": "/static/js/main.a7a63cc4.chunk.js"
+  },
+  {
+    "revision": "4475cebb610fb864266a",
+    "url": "/static/js/runtime-main.9567774f.js"
+  }
+]);
